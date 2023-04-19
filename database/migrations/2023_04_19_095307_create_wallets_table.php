f078da88cc43282cfbf87acbcaf51b3ed060417f
Valerii Koltsov <valeriy.k@zlogic.io>
2023-04-19 12:57:28 +0300

    added wallet model and mnigration

--- database/migrations/2023_04_19_095307_create_wallets_table.php
+++ database/migrations/2023_04_19_095307_create_wallets_table.php
@@ -11,12 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('wallets', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->foreignId('user_id')
+                ->constrained('users')
+                ->cascadeOnDelete();
+            $table->string('currency');
+            $table->decimal('amount', 10);
             $table->timestamps();
         });
     }
@@ -26,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('wallets');
     }
 };
