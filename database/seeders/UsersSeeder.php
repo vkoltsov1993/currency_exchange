519fb93b445ce446f7358116cf8c542a46eb81d3
Valerii Koltsov <valeriy.k@zlogic.io>
2023-04-19 13:06:43 +0300

    updated users seeder

--- database/seeders/UsersSeeder.php
+++ database/seeders/UsersSeeder.php
@@ -13,8 +13,10 @@ class UsersSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory()
-            ->count(10)
-            ->make();
+        if (!User::exists()){
+            User::factory()
+                ->count(10)
+                ->create();
+        }
     }
 }
