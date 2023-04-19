0cab298043b86252cf48bc9db1f0fa0891dd78d5
Valerii Koltsov <valeriy.k@zlogic.io>
2023-04-19 13:44:36 +0300

    updated ExchangeRequest model

--- app/Models/ExchangeRequest.php
+++ app/Models/ExchangeRequest.php
@@ -4,8 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ExchangeRequest extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'currency_give',
+        'amount_give',
+        'currency_get',
+        'amount_get',
+        'is_done',
+        'fee',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
